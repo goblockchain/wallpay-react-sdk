305ca83f46ace3f00cc2437aac1b4c9aaba7c428
mukaportes <mport.dev@gmail.com>
2022-08-12 17:32:58 -0300

    Exporting all hooks through build elements

--- src/build.tsx
+++ src/build.tsx
@@ -1,19 +1,29 @@
 import { PaymentModal } from "./components/PaymentModal";
-import { PaymentProvider } from "./hooks/usePayment";
+import * as useConfigHooks from "./hooks/useConfig";
+import * as useEthereumHooks from "./hooks/useEthereum";
+import * as useNotificationHooks from "./hooks/useNotification";
+import * as usePaymentHooks from "./hooks/usePayment";
+import * as useStoreHooks from "./hooks/useStore";
+import * as useWalletsHooks from "./hooks/useWallets";
 
 // TODO: receive router here to avoid dependency of nextjs
 const buildElements = (sdkPrivateKey: string) => {
   // TODO: verify how to pass along the private key
   return {
-    PaymentModal: (props: any) => (<PaymentModal 
-      onClose={props.onClose} 
-      paymentData={props.paymentData} 
+    ...usePaymentHooks,
+    PaymentModal: (props: any) => (<PaymentModal
+      onClose={props.onClose}
+      paymentData={props.paymentData}
       sdkPrivateKey={sdkPrivateKey}
-     />),
-    PaymentProvider: (children: any) => (<PaymentProvider 
+    />),
+    PaymentProvider: ({ children }) => (<usePaymentHooks.PaymentProvider
       sdkPrivateKey={sdkPrivateKey}
-      children={children}
-     />),
+    >{children}</usePaymentHooks.PaymentProvider>),
+    ...useConfigHooks,
+    ...useEthereumHooks,
+    ...useNotificationHooks,
+    ...useStoreHooks,
+    ...useWalletsHooks,
   };
 };
 
