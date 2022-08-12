c3c8d8567040d15f9d29d3578dcc1dce9419151a
mukaportes <mport.dev@gmail.com>
2022-08-12 17:32:29 -0300

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,77 @@
+import path from 'path';
+import fs from 'fs';
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import dts from "rollup-plugin-dts";
+import jsonPlugin from "@rollup/plugin-json";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import builtins from 'rollup-plugin-node-builtins';
 
 const packageJson = require("./package.json");
+
+function pngResolverPlugin() {
+  return {
+    name: 'png-resolver',
+    resolveId(source, importer) {
+      if (source.endsWith('.png')) {
+        return path.resolve(path.dirname(importer), source);
+      }
+    },
+    load(id) {
+      if (id.endsWith('.png')) {
+        const referenceId = this.emitFile({
+          type: 'asset',
+          name: path.basename(id),
+          source: fs.readFileSync(id)
+        });
+        return `export default import.meta.ROLLUP_FILE_URL_${referenceId};`;
+      }
+    }
+  };
+}
+
+function svgResolverPlugin() {
+  return {
+    name: 'svg-resolver',
+    resolveId(source, importer) {
+      if (source.endsWith('.svg')) {
+        return path.resolve(path.dirname(importer), source);
+      }
+    },
+    load(id) {
+      if (id.endsWith('.svg')) {
+        const referenceId = this.emitFile({
+          type: 'asset',
+          name: path.basename(id),
+          source: fs.readFileSync(id)
+        });
+        return `export default import.meta.ROLLUP_FILE_URL_${referenceId};`;
+      }
+    }
+  };
+}
+
+function nodeResolverPlugin() {
+  return {
+    name: 'node-resolver',
+    resolveId(source, importer) {
+      if (source.endsWith('.node')) {
+        return path.resolve(path.dirname(importer), source);
+      }
+    },
+    load(id) {
+      if (id.endsWith('.node')) {
+        const referenceId = this.emitFile({
+          type: 'asset',
+          name: path.basename(id),
+          source: fs.readFileSync(id)
+        });
+        return `export default import.meta.ROLLUP_FILE_URL_${referenceId};`;
+      }
+    }
+  };
+}
 
 export default [
   {
@@ -21,15 +89,23 @@ export default [
       },
     ],
     plugins: [
-      resolve(),
+      // peerDepsExternal(),
+      // resolve({
+      //   preferBuiltins: true
+      // }),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      pngResolverPlugin(),
+      svgResolverPlugin(),
+      nodeResolverPlugin(),
+      jsonPlugin(),
+      // builtins(),
     ],
+    external: ["react", "react-dom", "@chakra-ui/react"],
   },
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [packageJson.devDependencies],
   },
 ];
