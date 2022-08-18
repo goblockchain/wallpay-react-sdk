05de14796b2cf2d21cd96150c37f55d09f76d2e6
mukaportes <mport.dev@gmail.com>
2022-08-18 18:03:31 -0300

    Add stripe config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const WALLPAY_API_URL_DEV = 'http://localhost:8001'
 export const WALLPAY_API_URL = 'http://localhost:8001'
+export const STRIPE_SECRET_KEY='pk_test_51LXrofPR5Eq6G1ErWU5z3GZXyCmZE97s7Nsyieik0cWO4f4MYmhKkn9wjMcGWEbo6VNiLfDVIuIJNayZyhd0VpgL003X74mEkV'
