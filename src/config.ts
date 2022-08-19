225452e4fec2a6e21414bd27e79e1eddb49085a6
mukaportes <mport.dev@gmail.com>
2022-08-19 17:37:26 -0300

    Update config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const WALLPAY_API_URL_DEV = 'http://localhost:8001'
 export const WALLPAY_API_URL = 'http://localhost:8001'
-export const STRIPE_SECRET_KEY='pk_test_51LXrofPR5Eq6G1ErWU5z3GZXyCmZE97s7Nsyieik0cWO4f4MYmhKkn9wjMcGWEbo6VNiLfDVIuIJNayZyhd0VpgL003X74mEkV'
+export const STRIPE_SECRET_KEY='pk_test_51IVM5RAoNQ6TWrMbQzKtDywG1Bnqd0izgE1RQEwi9mke05Ly3vNojleC9NMiEsF909ThSEaiDlDfzT574bk3Jajs00SQYDa3aL'
