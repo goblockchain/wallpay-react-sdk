afa885a3fc09d4e3300e47cdb4b2aa32eb5e15e0
Pedro Renato <pedro.renato.mello@gmail.com>
2022-08-30 16:06:34 -0300

    New BuyButton component to simplify integration

--- src/hooks/useConfig.tsx
+++ src/hooks/useConfig.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { LOGIN_TYPE } from "@toruslabs/torus-embed";
 
 import { WalletProviders } from "./useWallets";
@@ -14,6 +14,7 @@ export type Config = {
   contractAddress: string;
   currency: "BRL" | "USD";
   mainColor: string;
+  sdkPrivateKey: string;
 };
 
 export interface IConfigContext {
