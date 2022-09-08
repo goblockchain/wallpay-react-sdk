db518af7486be47a9fcacd7af74cf75423962dcb
mukaportes <mport.dev@gmail.com>
2022-09-08 10:35:09 -0300

    Add ConnectButton component

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,16 +1,53 @@
-import { Box, ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider, ResponsiveValue } from '@chakra-ui/react';
+import { CSSProperties } from 'react';
 import { useWallets } from '../hooks/useWallets';
 import { theme } from '../styles/theme';
 import { ConnectBodyMetaMask, ConnectEtherWalletsButton } from './ConnectEtherWallets';
 
-export const ConnectButton = () => {
+type ConnectButtonProps = {
+  btnTextColor?: string;
+  btnBackgroundColor?: string;
+  btnBorderColor?: string;
+  fixedButton?: boolean;
+  sdkPrivateKey: string;
+};
+
+export const ConnectButton = ({
+  btnTextColor,
+  btnBackgroundColor,
+  btnBorderColor,
+  fixedButton = false,
+  sdkPrivateKey,
+}: ConnectButtonProps) => {
   const { onOpenModal, walletIsConnected } = useWallets()
+
+  let CustomBox = ({ children }: { children: any }) => (<Box>{children}</Box>);
+
+  if (fixedButton) {
+    CustomBox = ({ children }: { children: any }) => (<Box style={{ left: 16, bottom: 16 }} pos="fixed">{children}</Box>);
+  }
 
   return (
     <ChakraProvider resetCSS={false} theme={theme}>
-      <Box>
-        {walletIsConnected === true ? (<ConnectBodyMetaMask />) : (<ConnectEtherWalletsButton handleOpenWalletModal={onOpenModal} />)}
-      </Box>
+      <CustomBox>
+        {
+          walletIsConnected === true ?
+            (<ConnectBodyMetaMask
+              sdkPrivateKey={sdkPrivateKey}
+              btnTextColor={btnTextColor}
+              btnBackgroundColor={btnBackgroundColor}
+              btnBorderColor={btnBorderColor}
+              fixedButton={fixedButton}
+            />)
+            : (
+              <ConnectEtherWalletsButton
+                handleOpenWalletModal={onOpenModal}
+                btnTextColor={btnTextColor}
+                btnBackgroundColor={btnBackgroundColor}
+                btnBorderColor={btnBorderColor}
+              />)
+        }
+      </CustomBox>
     </ChakraProvider>
   )
 };
